991815a6d6bc32f57e497a7105d7c93edc3ba835
ankitsinghrana13 <ak9897432538@gmail.com>
2022-03-11 20:29:46 +0800

    Add project files.

---
+++ MyAdminpro/Models/MSDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAdminpro.Models
+{
+    public class MSDBContext:DbContext
+    {
+        public DbSet<TBL_Users> TBL_Users { get; set; }
+        public DbSet<VerifyAccount> VerifyAccounts { get; set; }
+
+protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(DbConnection.ConenctionStr);
+        }
+    }
+}
---
+++ MyAdminpro/Models/TBL_Users.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAdminpro.Models
+{
+    public class TBL_Users
+    {
+        public int id { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Gender { get; set; }
+        public bool IsActive { get; set; }
+        public string ProfileImage { get; set; }
+        public bool IsVerified { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public DateTime? UpdateOn { get; set; }
+
+    }
+}
